0f70757bf34771a862f9674fca396e3dac7f0257
锦此 <a275727449@gmail.com>
2022-06-30 16:59:09 +0800

    feat: image html
    第一版完成

--- web/setImages.js
+++ web/setImages.js
@@ -1,4 +1,22 @@
 var setImages = function(oFile, year){
-    let elmString = oFile[year  | "2016" ].imgs.map(img=> `<div class='one_img' style='background-image: url(../${year}/800/${img})'></div>`).join("")
+    let elmString = oFile[year  | "2016" ].imgs.map(img=> `<div class='one_img'><a download="${img}" href="../${year}/${img}"><img src="../${year}/800/${img}"/></a></div>`).join("")
+    // Promise.all(oFile[year  | "2016" ].imgs.map(img=> {
+    //     return new Promise((resolve,reject)=>{
+    //         let elm = new Image()
+    //         // elm.style.display = "none"
+    //         // elm.style.width = "100%"
+    //         // elm.style.height = "100%"
+    //         // document.body.appendChild(elm)
+    //         elm.onload = ()=>{
+    //             console.log(elm)
+    //             console.log(elm.clientWidth)
+    //         }
+    //         elm.src = `../${year}/800/${img}`
+
+    //         resolve(`<div class='one_img' style='background-image: url(../${year}/800/${img})'></div>`)
+    //     })
+    // })).then(rz=>{
+    //     console.log(rz)
+    // })
     window.ImageBox.innerHTML =`<div class="img_wrap">${elmString}</div>` 
 }
